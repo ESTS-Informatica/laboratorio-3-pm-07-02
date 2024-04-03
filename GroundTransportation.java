2fa419e13cdb500faf77081356a4b3bee2029abb
Daniel Varela <202300212@estudantes.ips.pt>
2024-04-03 09:18:38 +0100

    Nível 2
    Nível 2

--- GroundTransportation.java
+++ GroundTransportation.java
@@ -1,12 +1,17 @@
-public class GroundTransportation extends Transport
-{
+public class GroundTransportation extends Transport {
     private String licensePlate;
     private final static double FEES = 3.00;
 
-    public GroundTransportation(String licensePlate)
-    {
+    public GroundTransportation(String licensePlate) {
         this.licensePlate = licensePlate;
-        
     }
 
+    public String getLicensePlate() {
+        return this.licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+
+        this.licensePlate = licensePlate;
+    }
 }
